1a7f14f1b8739de56cb3b994574adad9bf2ac892
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-08-08 19:34:40 -0300

    feat: cria um objeto para facilitar a manipulação de tipos de máquinas na aplicação

---
+++ src/Shared/Models/MachType.js
@@ -0,0 +1,15 @@
+import Forkilift from "../../Assets/Images/Icons/Machines/ForkliftIcon.svg";
+import Excavator from "../../Assets/Images/Icons/Machines/ExcavatorIcon.svg";
+import Bulldozer from "../../Assets/Images/Icons/Machines/BulldozerIcon.svg";
+import Loader from "../../Assets/Images/Icons/Machines/LoaderIcon.svg";
+import Truck from "../../Assets/Images/Icons/Machines/TruckIcon.svg";
+import Crane from "../../Assets/Images/Icons/Machines/CraneIcon.svg";
+
+export const MachType = {
+  forkilift: { label: "Empilhadeira", icon: Forkilift, value: 1 },
+  exavator: { label: "Excavadeira", icon: Excavator, value: 2 },
+  bulldozer: { label: "Trator de Esteira", icon: Bulldozer, value: 3 },
+  loader: { label: "Carregadeira", icon: Loader, value: 4 },
+  truck: { label: "Trator", icon: Truck, value: 5 },
+  crane: { label: "Guindaste", icon: Crane, value: 6 },
+};
