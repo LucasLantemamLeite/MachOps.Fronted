6d4f25d8bbf2f9e0dd48a731dfdcbdf708d73e74
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-08-15 18:53:29 -0300

    feat: cria a estrutura da barra de navegação já usando os componentes génericos

--- src/Sections/NavBar/NavBar.jsx
+++ src/Sections/NavBar/NavBar.jsx
@@ -1,15 +1,26 @@
+import { Button } from "../../Ui/Button/Button";
 import { Image } from "../../Ui/Image/Image";
+import { Input } from "../../Ui/Input/Input";
 import "./NavBarStyle.scss";
 
 export function NavBar() {
   return (
     <>
       <nav>
-        <div className="navbar__div-applogo">
-          <Image staticImage={"AppLogoIcon"} widthIcon="150px" />
+        <div>
+          <Image staticImage={"AppLogoIcon"} widthIcon="170px" />
         </div>
-        <div className="navbar__div-searchbox">Caixa de busca</div>
-        <div className="navbar__div-addbtn">Botão adicionar</div>
+        <div className="navbar__div-searchbox">
+          <div className="navbar__div-icon-wrapper">
+            <Image staticImage={"SearchIcon"} widthIcon={"25px"} />
+          </div>
+          <Input placeholder={"Busque uma máquina aqui..."} />
+        </div>
+        <div className="navbar__div-addbtn">
+          <Button icon={"AddIcon"} widthIcon="20px">
+            Adicionar
+          </Button>
+        </div>
       </nav>
     </>
   );
