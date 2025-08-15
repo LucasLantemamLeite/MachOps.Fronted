06209524f3dbd30171b71c3aee76b7027f9418da
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-08-15 18:19:19 -0300

    feat: adiciona o novo icone de lupa no centralizador e altera o caminho do logo

--- src/Models/Icons.js
+++ src/Models/Icons.js
@@ -1,5 +1,7 @@
-import AppLogo from "../Assets/Icons/AppLogo.svg";
+import AppLogo from "../Assets/Icons/AppLogoIcon.svg";
+import Search from "../Assets/Icons/SearchIcon.svg";
 
 export const Icons = {
   AppLogoIcon: AppLogo,
+  SearchIcon: Search,
 };
