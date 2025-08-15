4147459e49f2f4c00a2f8c237b88b1755cc58082
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-08-15 18:51:40 -0300

    feat: adiciona na central de icones

--- src/Models/Icons.js
+++ src/Models/Icons.js
@@ -1,7 +1,9 @@
 import AppLogo from "../Assets/Icons/AppLogoIcon.svg";
 import Search from "../Assets/Icons/SearchIcon.svg";
+import Add from "../Assets/Icons/AddIcon.svg";
 
 export const Icons = {
   AppLogoIcon: AppLogo,
   SearchIcon: Search,
+  AddIcon: Add,
 };
