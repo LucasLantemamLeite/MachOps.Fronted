85f0934a68ab8ba458e4ea096fea50ec5fb86a1b
Lucas Lantemam Leite <lucaslantemam2005@gmail.com>
2025-08-15 17:52:12 -0300

    fix: altera o alt para também seguir o src

--- src/Components/Image/Image.jsx
+++ src/Components/Image/Image.jsx
@@ -3,5 +3,5 @@ import { Icons } from "../../Models/Icons";
 export function Image({ staticImage = null, type, imgKey = null, className = null, widthIcon = "20px" }) {
   const src = staticImage ? Icons?.[staticImage] : type?.[imgKey];
 
-  return <img className={className} src={src} alt={imgKey} width={widthIcon} />;
+  return <img className={className} src={src} alt={src} width={widthIcon} />;
 }
